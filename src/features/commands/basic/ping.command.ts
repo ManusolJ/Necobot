c9a837c91523b3ec1ad4a44c51abf0a8276d72f0
Manu <manusolerj@gmail.com>
2026-06-18 16:59:56 +0200

    feat: add test Ping command to respond with Pong

---
+++ src/features/commands/basic/ping.command.ts
@@ -0,0 +1,31 @@
+import { env } from "@infrastructure/config/env.js";
+
+import type { ChatInputCommandInteraction } from "discord.js";
+
+import type {
+  Awaitable,
+  ApplicationCommandRegistry,
+} from "@sapphire/framework";
+import { Command } from "@sapphire/framework";
+
+const options = env.DISCORD_DEV_GUILD_ID
+  ? { guildIds: [env.DISCORD_DEV_GUILD_ID] }
+  : undefined;
+
+export class PingCommand extends Command {
+  public override registerApplicationCommands(
+    registry: ApplicationCommandRegistry,
+  ): Awaitable<void> {
+    registry.registerChatInputCommand(
+      (builder) =>
+        builder.setName("ping").setDescription("Replies with Pong (TEST)."),
+      options,
+    );
+  }
+
+  public override async chatInputRun(
+    interaction: ChatInputCommandInteraction,
+  ): Promise<void> {
+    await interaction.reply("Pong!");
+  }
+}
