1c9e7d1e60916ac216d4ac96dd704f0366792e07
Manu <manusolerj@gmail.com>
2026-06-18 15:48:47 +0200

    fix: correct database default path in config.constants.ts

--- src/shared/consts/config.constants.ts
+++ src/shared/consts/config.constants.ts
@@ -7,4 +7,4 @@ export const DEBUG_LOG_LEVEL = "debug";
 export const REDIS_DEFAULT_PORT = "6379";
 export const REDIS_DEFAULT_HOST = "127.0.0.1";
 
-export const DATABASE_DEFAULT_PATH = "./db/data.sqlite";
+export const DATABASE_DEFAULT_PATH = "./db/data/data.sqlite";
