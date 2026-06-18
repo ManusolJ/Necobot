45d18419c918728ac9487fd82a2eb07e1f8a2864
Manu <manusolerj@gmail.com>
2026-06-18 13:10:13 +0200

    feat: add drizzle configuration for database setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import "dotenv";
+import "dotenv/config";
 
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "sqlite",
   out: "./db/migrations",
-  schema: "./src/infrastructure/database/schema/*.ts",
+  schema: "./src/infrastructure/database/schema.ts",
   dbCredentials: {
     url: process.env.DATABASE_PATH ?? "./db/data.sqlite",
   },
