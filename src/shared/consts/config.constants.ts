c32a038ba973789c19eb951a9d0fb813bcbaa897
Manu <manusolerj@gmail.com>
2026-06-30 11:43:57 +0200

    fix: remove PROD_ENV and INFO_LOG_LEVEL constants from configuration

--- src/shared/consts/config.constants.ts
+++ src/shared/consts/config.constants.ts
@@ -1,7 +1,5 @@
 export const DEV_ENV = "dev";
-export const PROD_ENV = "prod";
 
-export const INFO_LOG_LEVEL = "info";
 export const DEBUG_LOG_LEVEL = "debug";
 
 export const REDIS_DEFAULT_PORT = "6379";
