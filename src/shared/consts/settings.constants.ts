df1e386068c8887d9a11a8e2962df9767ca9a44e
Manu <manusolerj@gmail.com>
2026-07-08 13:20:32 +0200

    feat: add settings constants for available prefixes and channel purposes

---
+++ src/shared/consts/settings.constants.ts
@@ -0,0 +1,13 @@
+export const AVAILABLE_PREFIXES = [
+  { name: "!", value: "!" },
+  { name: "?", value: "?" },
+  { name: ">", value: ">" },
+  { name: ".", value: "." },
+];
+
+export const CHANNEL_PURPOSES = [
+  { name: "Canal del Bot", value: "bot" },
+  { name: "Canal de Memes", value: "memes" },
+  { name: "Canal de Copypastas", value: "copypasta" },
+  { name: "Canal de Anuncios", value: "announcements" },
+];
