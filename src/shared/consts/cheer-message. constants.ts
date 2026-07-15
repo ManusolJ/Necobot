35a4c7603d4303ed7d89d302bec7a9d34b336057
Manu <manusolerj@gmail.com>
2026-07-15 20:48:50 +0200

    feat: add CHEER_MESSAGES constants for customizable birthday messages

---
+++ src/shared/consts/cheer-message. constants.ts
@@ -0,0 +1,6 @@
+export const CHEER_MESSAGES: readonly string[] = [
+  "¡Feliz cumpleaños, {user}! Nyaha~ hoy te perdono la miseria. Disfruta tu día, mortal.",
+  "¡Felicidades, {user}! Un año más cerca de... bueno, ya sabes. ¡A celebrar!",
+  "Burunya~ ¡feliz cumpleaños, {user}! Hoy el caos te sonríe. Aprovecha.",
+  "¡{user}, feliz cumpleaños! Mi regalo es esta imagen. La factura llega mañana.",
+];
