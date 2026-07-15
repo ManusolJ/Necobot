806022de539a8f4f83f6a31b3c9d270e851573ef
Manu <manusolerj@gmail.com>
2026-07-15 20:49:04 +0200

    feat: add minefield constants for game mechanics configuration

---
+++ src/shared/consts/minefield.constants.ts
@@ -0,0 +1,7 @@
+export const MINE_COST = 20;
+export const MAX_MINES_PER_PURCHASE = 10;
+
+export const MINE_TRIGGER_CHANCE = 0.02;
+
+export const MINE_TIMEOUT_MS = 30_000;
+export const MINE_ADMIN_PENALTY = 50;
