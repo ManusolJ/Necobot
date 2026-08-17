710ab40207973c44d3f8d09d20ec9eeea4a18c84
Manu <manusolerj@gmail.com>
2026-08-17 14:54:13 +0200

    Refactor: Remove GuildSettingsNotFoundError class from domain.errors.ts

--- src/infrastructure/errors/domain.errors.ts
+++ src/infrastructure/errors/domain.errors.ts
@@ -1,11 +1,5 @@
 import { AppError } from "./app-error.js";
 
-export class GuildSettingsNotFoundError extends AppError {
-  public constructor(guildId: string) {
-    super("guild_settings_not_found", { context: { guildId } });
-  }
-}
-
 export class GuildSettingsPersistError extends AppError {
   public constructor(guildId: string, cause?: unknown) {
     super("guild_settings_persist_failed", { context: { guildId }, cause });
