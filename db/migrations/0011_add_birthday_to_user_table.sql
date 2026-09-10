8bd4790fc4265a09454244cfd336c47ee9ca6a6f
Manu <manusolerj@gmail.com>
2026-09-10 14:25:31 +0200

    feat: add birthday fields to guild_users table and update migration journal

---
+++ db/migrations/0011_add_birthday_to_user_table.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `guild_users` ADD `birthday_day` integer;--> statement-breakpoint
+ALTER TABLE `guild_users` ADD `birthday_month` integer;--> statement-breakpoint
+ALTER TABLE `guild_users` ADD `birthday_cheered_year` integer;--> statement-breakpoint
+ALTER TABLE `guild_users` ADD `birthday_warned_year` integer;
